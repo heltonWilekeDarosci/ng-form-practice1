e23a4e2dbcd321d2ec722f97dbaede3103b38260
helton <heltoloco@hotmail.com>
2022-08-31 21:05:33 -0300

    feat(app/form): create form and routing

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent {
+
+  submit(regstr: any) {
+    console.log('Form submitted');
+  }
+
+}
